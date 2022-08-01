c84c00a0d9e8b7bdc43cf02768b9b6c838d92f84
Lambels <patrick.arvatu@yahoo.com>
2022-08-01 22:05:37 +0300

    simplified code

--- oauth/oauth2.go
+++ oauth/oauth2.go
@@ -3,42 +3,23 @@ package oauth
 import (
 	"net/http"
 
-	"github.com/Lambels/autho/internal"
 	"golang.org/x/oauth2"
 )
 
-type app struct {
-	callbackURL string
-	loginURL    string
-	// the oauth2 configuration.
-	config *oauth2.Config
+func NewLoginHandler(cfg *oauth2.Config) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
 
-	// entrypoint for: callbackURL
-	// -> callbackHandler / failiureHandler
-	// responsible for validating state and obtaining token and passing token to callback handler.
-	tokenHandler internal.Middleware
-	// responsible for token exchange.
-	// -> successHandler / failiureHandler
-	callbackHandler internal.Middleware
-
-	// entrypoint for: loginURL
-	// responsible for setting state cookie and redirecting flow to oauth2 provider.
-	loginHandler http.HandlerFunc
+	}
 }
 
-func (a *app) register(mux multiplexer) {
-	// the url which the user hits and chains of the oauth2 flow.
-	mux.HandleFunc(a.loginURL, a.loginHandler)
+func NewTokenHandler(cfg *oauth2.Config, errHandler, next http.Handler) http.Handler {
+	if errHandler == nil {
+		errHandler = DefaultFailureHandle
+	}
 
-	// the callback url.
-	mux.HandleFunc(a.callbackURL)
-}
+	fn := func(w http.ResponseWriter, r *http.Request) {
+		next.ServeHTTP(w, r)
+	}
 
-// loginHandler is the default oauth2 handler for the login URL.
-func loginHandler(w http.ResponseWriter, r *http.Request) {
-
-}
-
-func failiureHandler(w http.ResponseWriter, r *http.Request) {
-
+	return http.HandlerFunc(fn)
 }
