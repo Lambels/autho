c97877da52cef3ea9a10037c4c77cee3f46378b3
Lambels <patrick.arvatu@yahoo.com>
2022-08-03 13:21:50 +0300

    docs + helper method

--- google/oauth2.go
+++ google/oauth2.go
@@ -8,15 +8,48 @@ import (
 	googleOauth "google.golang.org/api/oauth2/v2"
 )
 
+// NewDefaultCallbackHandler is a helper function that constructs
+// a new callback handler using the default token handler google.NewTokenHandler()
+// wrapped arround the default google.NewUserHandler().
+//
+// This method saves allot of boilerplate. For more customisable handlers construct your
+// own callback handler by wrapping it around your own specific token handler.
+func NewDefaultCallbackHandler(oauthCfg *oauth2.Config, errHandler, terminalHandler http.Handler) http.Handler {
+	return NewTokenHandler(
+		oauthCfg,
+		errHandler,
+		NewUserHandler(
+			oauthCfg,
+			errHandler,
+			terminalHandler,
+		),
+	)
+}
+
+// NewLoginHandler creates a new LoginHandler which is resposible for setting a random
+// value (state) to the request context and state cookie. Afterwards the login handler is also
+// responsible for redirecting the user to the provider for the users grant.
 func NewLoginHandler(ckCfg *autho.CookieConfig, oauthCfg *oauth2.Config) http.Handler {
 	return autho.NewLoginHandler(ckCfg, oauthCfg)
 }
 
-func NewTokenHandler(cfg *oauth2.Config, errHandler, callbackHandler http.Handler) http.Handler {
-	return autho.NewTokenHandler(cfg, errHandler, callbackHandler)
+// NewTokenHandler creates a new TokenHandler which is the first handler in the chain responding
+// to the callback from the provider, it is responsible for parsing the response for auth code
+// and state then comparing the ctx state with the request state. Following the parsing the
+// TokenHandler performs the token exchange and adds the token to the request context, calling on
+// success the UserHandler.
+func NewTokenHandler(cfg *oauth2.Config, errHandler, userHandler http.Handler) http.Handler {
+	return autho.NewTokenHandler(cfg, errHandler, userHandler)
 }
 
-func NewCallbackHandler(cfg *oauth2.Config, errHandler, terminalHandler http.Handler) http.Handler {
+// NewUserHandler creates a new google UserHandler resposnible for using the tokens provided
+// by the TokenHandler in exchange for the users resource. The user resource is set under the
+// request context.
+//
+//	user, ok := autho.UserFromContext(r.Context()).(*oauth2.Userinfo)
+//
+// The UserModel used by default by the google.NewUserHandler is: https://pkg.go.dev/google.golang.org/api/oauth2/v2#Userinfo
+func NewUserHandler(cfg *oauth2.Config, errHandler, terminalHandler http.Handler) http.Handler {
 	if errHandler == nil {
 		errHandler = autho.DefaultFailureHandle
 	}
