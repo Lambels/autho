d0b83e239ea9e032f8b933a58bdbafc101b9a5fd
Lambels <patrick.arvatu@yahoo.com>
2022-08-03 22:16:18 +0300

    implemented oauth1

---
+++ oauth1/oauth1.go
@@ -0,0 +1,81 @@
+package oauth1
+
+import (
+	"net/http"
+
+	"github.com/Lambels/autho"
+	"github.com/dghubble/oauth1"
+)
+
+func NewLoginHandler(cfg *oauth1.Config, ckCfg *autho.CookieConfig, errHandler http.Handler) http.Handler {
+	if errHandler == nil {
+		errHandler = autho.DefaultFailureHandle
+	}
+
+	f := func(w http.ResponseWriter, r *http.Request) {
+		reqToken, reqSecret, err := cfg.RequestToken()
+		if err != nil {
+			r = r.WithContext(autho.ContextWithError(r.Context(), err))
+			errHandler.ServeHTTP(w, r)
+			return
+		}
+
+		authURL, err := cfg.AuthorizationURL(reqToken)
+		if err != nil {
+			r = r.WithContext(autho.ContextWithError(r.Context(), err))
+			errHandler.ServeHTTP(w, r)
+			return
+		}
+
+		if ckCfg != nil {
+			ck := autho.GetCookie(ckCfg, r)
+			ck.Value = reqSecret
+			http.SetCookie(w, ck)
+		}
+		r = r.WithContext(ContextWithRequestSecret(r.Context(), reqSecret))
+		http.Redirect(w, r, authURL.String(), http.StatusFound)
+	}
+
+	return http.HandlerFunc(f)
+}
+
+func NewTokenHandler(cfg *oauth1.Config, ckCfg *autho.CookieConfig, errHandler, userHandler http.Handler) http.Handler {
+	if errHandler == nil {
+		errHandler = autho.DefaultFailureHandle
+	}
+
+	f := func(w http.ResponseWriter, r *http.Request) {
+		reqToken, verifier, err := oauth1.ParseAuthorizationCallback(r)
+		if err != nil {
+			r = r.WithContext(autho.ContextWithError(r.Context(), err))
+			errHandler.ServeHTTP(w, r)
+			return
+		}
+
+		var reqSecret string
+		if ckCfg != nil {
+			ck, ckErr := r.Cookie(ckCfg.Name)
+			reqSecret, err = ck.Value, ckErr
+		} else {
+			reqSecret, err = RequestSecretFromContext(r.Context())
+		}
+		if err != nil {
+			r = r.WithContext(autho.ContextWithError(r.Context(), err))
+			errHandler.ServeHTTP(w, r)
+			return
+		}
+
+		accessToken, accessSecret, err := cfg.AccessToken(reqToken, reqSecret, verifier)
+		if err != nil {
+			r = r.WithContext(autho.ContextWithError(r.Context(), err))
+			errHandler.ServeHTTP(w, r)
+			return
+		}
+
+		tkn := oauth1.NewToken(accessToken, accessSecret)
+		r = r.WithContext(RequestWithToken(r.Context(), tkn))
+		userHandler.ServeHTTP(w, r)
+	}
+
+	return http.HandlerFunc(f)
+}
