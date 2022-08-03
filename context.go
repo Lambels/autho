b4aadfd8f512252fc67573bebd393d1919108a0b
Lambels <patrick.arvatu@yahoo.com>
2022-08-03 22:15:35 +0300

    moved specific methods

--- context.go
+++ context.go
@@ -2,9 +2,6 @@ package autho
 
 import (
 	"context"
-	"errors"
-
-	"golang.org/x/oauth2"
 )
 
 type errKey struct{}
@@ -35,34 +32,4 @@ func ContextWithUser(ctx context.Context, user interface{}) context.Context {
 // fails no user is set.
 func UserFromContext(ctx context.Context) interface{} {
 	return ctx.Value(userKey{})
-}
-
-type stateKey struct{}
-
-func ContextWithState(ctx context.Context, buf []byte) context.Context {
-	return context.WithValue(ctx, stateKey{}, buf)
-}
-
-func StateFromContext(ctx context.Context) ([]byte, error) {
-	state, ok := ctx.Value(stateKey{}).([]byte)
-	if !ok {
-		return []byte{}, errors.New("state parameter not set")
-	}
-
-	return state, nil
-}
-
-type tokenKey struct{}
-
-func ContextWithToken(ctx context.Context, tkn *oauth2.Token) context.Context {
-	return context.WithValue(ctx, tokenKey{}, tkn)
-}
-
-func TokenFromContext(ctx context.Context) (*oauth2.Token, error) {
-	tkn, ok := ctx.Value(tokenKey{}).(*oauth2.Token)
-	if !ok {
-		return &oauth2.Token{}, errors.New("token parameter not set")
-	}
-
-	return tkn, nil
 }
