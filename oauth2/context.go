1ec630035e6179b9d5615ce7f5488a6ab8722946
Lambels <patrick.arvatu@yahoo.com>
2022-08-04 18:03:49 +0300

    removed unused methods + docs

--- oauth2/context.go
+++ oauth2/context.go
@@ -7,31 +7,21 @@ import (
 	"golang.org/x/oauth2"
 )
 
-type stateKey struct{}
-
-func ContextWithState(ctx context.Context, buf []byte) context.Context {
-	return context.WithValue(ctx, stateKey{}, buf)
-}
-
-func StateFromContext(ctx context.Context) ([]byte, error) {
-	state, ok := ctx.Value(stateKey{}).([]byte)
-	if !ok {
-		return []byte{}, errors.New("state parameter not set")
-	}
-
-	return state, nil
-}
-
 type tokenKey struct{}
 
+// ContextWithToken is used by the token handler (default: oauth2.NewTokenHandler()) to
+// set the oauth2 tokens under the context to be used by the user handler in exchange for
+// the user resource.
 func ContextWithToken(ctx context.Context, tkn *oauth2.Token) context.Context {
 	return context.WithValue(ctx, tokenKey{}, tkn)
 }
 
+// TokenFromContext is used to harvest the tokens from the request context by the
+// user handler and preform the token/user-resource exchange.
 func TokenFromContext(ctx context.Context) (*oauth2.Token, error) {
 	tkn, ok := ctx.Value(tokenKey{}).(*oauth2.Token)
 	if !ok {
-		return &oauth2.Token{}, errors.New("token parameter not set")
+		return &oauth2.Token{}, errors.New("autho: token parameter not set")
 	}
 
 	return tkn, nil
