f0a5c6bb1e34b167dd4cf6fca4d5ac570a499840
Lambels <patrick.arvatu@yahoo.com>
2022-08-04 18:02:16 +0300

    added docs + error return

--- context.go
+++ context.go
@@ -2,10 +2,12 @@ package autho
 
 import (
 	"context"
+	"errors"
 )
 
 type errKey struct{}
 
+// ContextWithError adds err to the context to be used by the err handler.
 func ContextWithError(ctx context.Context, err error) context.Context {
 	return context.WithValue(ctx, errKey{}, err)
 }
@@ -23,13 +25,20 @@ func ErrorFromContext(ctx context.Context) error {
 
 type userKey struct{}
 
+// ContextWithUser adds the user to the context to be used by the terminal handler.
 func ContextWithUser(ctx context.Context, user interface{}) context.Context {
 	return context.WithValue(ctx, userKey{}, user)
 }
 
-// UserFromContext retrieves the user struct under the user key.
-// When the struct is returned parse it to the specific user you are expecting, if the parcing
-// fails no user is set.
-func UserFromContext(ctx context.Context) interface{} {
-	return ctx.Value(userKey{})
+// UserFromContext harvests the user struct from the request context.
+// When the struct is returned if no error is present
+// parse it to the specific user you are expecting from the user handler (check your providers
+// user handler for extra info)
+func UserFromContext(ctx context.Context) (interface{}, error) {
+	user := ctx.Value(userKey{})
+	if user == nil {
+		return nil, errors.New("autho: user parameter not ser")
+	}
+
+	return user, nil
 }
