352af29f07c686667880ba515257a491c58c0e3d
Lambels <patrick.arvatu@yahoo.com>
2022-08-04 18:03:20 +0300

    added docs

--- oauth1/context.go
+++ oauth1/context.go
@@ -9,14 +9,19 @@ import (
 
 type tokenKey struct{}
 
-func RequestWithToken(ctx context.Context, token *oauth1.Token) context.Context {
-	return context.WithValue(ctx, tokenKey{}, token)
+// RequestWithToken is used by the token handler (default: oauth1.NewTokenHandler()) to
+// set the oauth1 tokens under the context to be used by the user handler in exchange for
+// the user resource.
+func RequestWithToken(ctx context.Context, tkn *oauth1.Token) context.Context {
+	return context.WithValue(ctx, tokenKey{}, tkn)
 }
 
+// TokenFromContext is used to harvest the tokens from the request context by the
+// user handler and preform the token/user-resource exchange.
 func TokenFromContext(ctx context.Context) (*oauth1.Token, error) {
 	tkn, ok := ctx.Value(tokenKey{}).(*oauth1.Token)
 	if !ok {
-		return nil, errors.New("token not ser")
+		return &oauth1.Token{}, errors.New("autho: token parameter not set")
 	}
 
 	return tkn, nil
